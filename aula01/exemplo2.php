dbf9e0d22e44956cd7aeabe7c2a3de8adb041f94
Goht <161866585+pedromiranda01@users.noreply.github.com>
2026-02-10 17:30:52 -0300

    Add files via upload

---
+++ aula01/exemplo2.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = "Anderson";
+    $idade = "25";
+    $preco = "10.50";
+    $ativo = "true";
+    echo "Nome: " . $nome . "<br>";
+    echo "Idade: " . $nome . "<br>";
+    echo "Preço: " . $nome . "<br>";
+    echo "Status: " . ($ativo ? "Ativo" : "Inativo");
+
+?>
